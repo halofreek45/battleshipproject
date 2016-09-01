facd90fa5cfef5d0d1904fe91a6ea3ea17986120
Propa Panda <halofreek45@gmail.com>
2016-09-01 14:35:38 -0700

    Working on stuffffffff
    I am using Substring to seperate the user input and get The letter and
    turn it into a number which we can then use to look up the box they are
    targeting in the arrays/gameboard!!!!! ΩΩΩΩΩΩΩΩΩΩ ω ω ω ω ω ω ω ω ω ω ω
    ω ω ω ω ϰ ϰ ϰ ϰ ϰ ϰ ϰ ϰ ϰ ϰ

--- battleship.js
+++ battleship.js
@@ -5,7 +5,6 @@ var squareSize = 50;
 
 // gets the container element
 var gameBoardContainer = document.getElementById("gameboard");
-
 // you can use this to convert your letters into numbers for use
 // with the 2D array
 var letterConversion = {
@@ -24,16 +23,22 @@ var letterConversion = {
 // makes the grid columns and rows
 for (i = 0; i < cols; i++) {
 	for (j = 0; j < rows; j++) {
-
+////Use i for Columns and j for rows////
 		// creates a new div HTML element for each grid square and makes it the right size
 		var square = document.createElement("div");
 		gameBoardContainer.appendChild(square);
 
     // give each div element a unique id based on its row and column, like "s00"
+
 		square.id = 's' + j + i;
 		square.className = "boardSquare";
+  //console.log(square.id);
 
-		// THIS IS WHERE YOU WILL ADD CODE FOR PART 1 TO ADD TEXT TO EACH SQUARE
+var columnLetter = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"]
+
+var rowDisplay = i + 1;
+
+	  square.textContent = columnLetter[j] + rowDisplay;
 
 		// set each grid square's coordinates: multiples of the current row or column number
 		var topPosition = j * squareSize;
@@ -59,8 +64,24 @@ var gameBoard = [
 				[1,0,0,0,0,0,0,0,0,0]
 				]
 
+var fireInput;
+var inputColumn;
+var inputRow;
+var targetedBox;
+var firstInput;
+var secondInput
+
+//console.log(gameBoard[][]);
+
 function fireTorpedo() {
 
-	// Your game logic will go here!
+console.log("Click Function works");
+
+fireInput = document.getElementById("gameBoardInput").value;
+firstInput = fireInput.substring(0, 1);
+secondInput = fireInput.substring(1, 2);
+console.log(firstInput);
+//inputColumn = letterConversion.J; // This logs 9 which coressponds in the letterConversion but how do you use a Variable instead of a letter?
 
 }
+//Add Latin Numbers :) Maybe as an end thing?
